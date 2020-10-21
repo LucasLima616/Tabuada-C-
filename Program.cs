a11206ff456ae7db607c6ea50ea57e93c0e1331c
Lucas Lima <67014560+LucasLima616@users.noreply.github.com>
2020-10-21 16:17:41 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exerciocio_.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int num;
+        Console.Write("Digite um valor inteiro :");
+        
+        num = Convert.ToInt16(Console.ReadLine());
+        //enquanto o valor de ir for menor que 10 fazemos o numero digitado VEZES o valor de i "já iniciado com 1"
+       // ou seja entramos com o valor 10. na primeira volta do FOR fica 10 * 1 = 10 ...
+        for (int i = 1; i <= 10; i++)
+        {
+        //escrevemos os valores na tela.
+            Console.WriteLine(num + " x " + i + " = " + num * i);
+ 
+        }
+        //espera uma tecla ser pressionada
+        Console.ReadKey();
+    }
+}
+}
